d76a0bc7f790df25cbbb36040248271909ba156c
Leonardo Guerreiro <guerreiroleo.py@gmail.com>
2025-06-05 09:46:12 -0300

    add first stream

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -11,3 +11,28 @@
 // 10mb/s -> 10.000
 
 // Readable Streams (CSV) / Writable Streams (Netflix)
+
+// Streams --> Fluxo de dados
+
+// process.stdin.pipe(process.stdout);
+
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    if (i > 100) {
+      this.push(null);
+    } else {
+      setTimeout(() => {
+        const buf = Buffer.from(String(i));
+        this.push(buf);
+      }, 1000);
+    }
+  }
+}
+
+new OneToHundredStream().pipe(process.stdout);
