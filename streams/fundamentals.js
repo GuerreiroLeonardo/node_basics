c802d55d427ad87ce86f7512db89a5f01dabd73f
Leonardo Guerreiro <guerreiroleo.py@gmail.com>
2025-06-05 09:39:26 -0300

    explaining streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,13 @@
+// Netflix & SpotifyAdd commentMore actions
+
+// Importação de clientes via CSV (Excel)
+// 1gb - 1.000.000
+// POST /upload import.csv
+
+// 10mb/s - 100s
+
+// 100s -> Inserção no banco de dados
+
+// 10mb/s -> 10.000
+
+// Readable Streams (CSV) / Writable Streams (Netflix)
