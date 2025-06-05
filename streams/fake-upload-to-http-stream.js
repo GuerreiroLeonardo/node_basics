39f496c08fdc5d6680f1ba9a0b13fb18ce40b2c2
Leonardo Guerreiro <guerreiroleo.py@gmail.com>
2025-06-05 10:09:08 -0300

    applying streams o http

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,25 @@
+import { Readable } from "node:stream";
+export class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    if (i > 100) {
+      this.push(null);
+    } else {
+      setTimeout(() => {
+        const buf = Buffer.from(String(i));
+        this.push(buf);
+      }, 1000);
+    }
+  }
+}
+
+fetch("http://localhost:3334", {
+  method: "POST",
+  body: new OneToHundredStream(),
+  duplex: "half",
+}).then((response) => {
+  console.log(response);
+});
